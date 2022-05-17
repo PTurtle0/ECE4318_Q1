64389785ae46d555db9ef5f41ab81e1b22bcfef0
PTurtle0 <67627759+PTurtle0@users.noreply.github.com>
2022-05-17 16:11:55 -0700

    Add files via upload

---
+++ gaussian_dist.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.integrate as integrate
+
+def cdf(x):
+    cdf_constant = 1.0 / np.sqrt(2*np.pi)
+    cdf_integral = np.e ** ((-x**2) / 2.0)
+    return (cdf_constant * cdf_integral)
+
+z = np.arange(0, 4.99, 0.01, dtype=float)
+phi = []
+
+for i in z:
+    x = z[i]
+    output = integrate.trapezoid(cdf, np.NINF, x)
+    phi[i] = output
+
+print(phi)
+    
+
